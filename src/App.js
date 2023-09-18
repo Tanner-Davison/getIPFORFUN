927f87f0e27e454a3c9f66ee045d9b7607b80e16
tannerdavison <tboydavison@gmail.com>
2023-09-18 00:26:13 -0600

    set up axios calls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import axios from 'axios'
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [stateYoureIn,setStateYoureIn] = useState('')
+const [location, setLocation] = useState('')
+  const getLocalIpInfo = (event) => {
+    event.preventDefault();
+    axios
+      .get("https://ipinfo.io?token=6b922a43d53dac")
+      .then(res => {
+        console.log(res.data)
+        setStateYoureIn(res.data.country);
+       d
+      })
+      .then(res => {
+        setLocation({
+          ip: res.data.ip,
+          region: res.data.region,
+          postal: res.data.postal
+      })
+      })
+    console.log(stateYoureIn, location)
+  }
+	return (
+		<div className='App'>
+			<form onSubmit={getLocalIpInfo}>
+				<button>Click Me</button>
+				<input />
+
+				<button type='submit'>Submit</button>
+				<input />
+			</form>
+		</div>
+	);
 }
 
 export default App;
